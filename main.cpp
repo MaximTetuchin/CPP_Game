6bbffd4005504ea03d48419a205895d299ecbb63
Mazer1x <tetuhin@inbox.ru>
2025-03-04 16:29:11 +0300

    added direction of view variable and animation

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ const int W = 40;
 String TileMap[H] = {
 
 "                                        ",
-"B                                B     B",
+"B                                      B",
 "B                                B     B",
 "B                                      B",
 "B                                      B",
@@ -44,71 +44,74 @@ class Entity {
 			sprite.setPosition(rect.left, rect.top);
 			window.draw(sprite);
 		}
-	};
+};
 	
 
+class Player : public Entity {
+	public:
+		bool onGround;
+		//! Animation fields
+		float currentFrame;
+		float idleFrame;
+		bool lastAction; //! i enter this field to understand at which direction player is currently looking.
+		//! 1 - forward, 0 - backward	
 
-
-	class Player : public Entity {
-		public:
-			bool onGround;
-			float currentFrame;
-			float idleFrame;
+	
+		Player(Texture &image) : Entity(image, 7 * 32, 9 * 32, 40, 50) {
+			onGround = false;
+			currentFrame = 0;
+			lastAction = 1;
+		}
+	
+		void update(float time) override {
+			rect.left += dx * time;
+			Collision(0);
 		
-			Player(Texture &image) : Entity(image, 7 * 32, 9 * 32, 40, 50) {
-				onGround = false;
-				currentFrame = 0;
-			}
+			if (!onGround) dy += 0.0005 * time;
+			rect.top += dy * time;
+			onGround = false;
+			Collision(1);
 		
-			void update(float time) override {
-				rect.left += dx * time;
-				Collision(0);
-			
-				if (!onGround) dy += 0.0005 * time;
-				rect.top += dy * time;
-				onGround = false;
-				Collision(1);
-			
-				currentFrame += 0.005 * time;
-				idleFrame = currentFrame/1.5;
-				if (currentFrame > 6) currentFrame -= 6;
-				if (idleFrame > 3) idleFrame -= 3; 
+			currentFrame += 0.005 * time;
+			idleFrame = currentFrame/1.5; //! idle animation must be slower than the original one, i think so..
 
-				if (dx > 0) {
-					sprite.setTextureRect(IntRect(40 * int(currentFrame), 244, 40, 50));
-				}
+			if (currentFrame > 6) currentFrame -= 6;
+			if (idleFrame > 3) idleFrame -= 3; 
 
-				if (dx < 0) {
-					sprite.setTextureRect(IntRect(40 * int(currentFrame) + 40, 244, -40, 50));
-				}
-				
-
-				if (dx == 0) {
+			if (dx > 0) {
+				sprite.setTextureRect(IntRect(40 * int(currentFrame), 244, 40, 50));
+			} else if (dx < 0) {
+				sprite.setTextureRect(IntRect(40 * int(currentFrame) + 40, 244, -40, 50));
+			} else if (dx == 0) {
+				if (lastAction){
 					sprite.setTextureRect(IntRect(43 * int(idleFrame)+4, 188, 40, 50));
+				} else {
+					sprite.setTextureRect(IntRect(43 * int(idleFrame)+4 + 40, 188, -40, 50));
 				}
-			
-				sprite.setPosition(rect.left, rect.top);
-			
-				dx = 0;
 			}
-					
 		
+			sprite.setPosition(rect.left, rect.top);
 		
-			void Collision(int dir) {
-				for (int i = rect.top / 32; i < (rect.top + rect.height) / 32; i++)
-					for (int j = rect.left / 32; j < (rect.left + rect.width) / 32; j++) {
-						if (TileMap[i][j] == 'B') {
-							if ((dx > 0) && (dir == 0)) rect.left = j * 32 - rect.width;
-							if ((dx < 0) && (dir == 0)) rect.left = j * 32 + 32;
-							if ((dy > 0) && (dir == 1)) { rect.top = i * 32 - rect.height; dy = 0; onGround = true; }
-							if ((dy < 0) && (dir == 1)) { rect.top = i * 32 + 32; dy = 0; }
-						}
-						if (TileMap[i][j] == '0'){
-							TileMap[i][j] = ' ';
-						}
+			dx = 0;
+		}
+				
+	
+	
+		void Collision(int dir) {
+			for (int i = rect.top / 32; i < (rect.top + rect.height) / 32; i++)
+				for (int j = rect.left / 32; j < (rect.left + rect.width) / 32; j++) {
+					if (TileMap[i][j] == 'B') {
+						if ((dx > 0) && (dir == 0)) rect.left = j * 32 - rect.width;
+						if ((dx < 0) && (dir == 0)) rect.left = j * 32 + 32;
+						if ((dy > 0) && (dir == 1)) { rect.top = i * 32 - rect.height; dy = 0; onGround = true; }
+						if ((dy < 0) && (dir == 1)) { rect.top = i * 32 + 32; dy = 0; }
 					}
-			}
-		};		
+					if (TileMap[i][j] == '0'){
+						TileMap[i][j] = ' ';
+					}
+				}
+		}
+	};		
 
 int main()
 {   
@@ -148,10 +151,12 @@ int main()
 	
 		if (Keyboard::isKeyPressed(Keyboard::Left)) {
 			p.dx = -0.1;
+			p.lastAction = 0;
 		}
 	
 		if (Keyboard::isKeyPressed(Keyboard::Right)) {
 			p.dx = 0.1;
+			p.lastAction = 1;
 		}
 	
 		if (Keyboard::isKeyPressed(Keyboard::Up)) {
