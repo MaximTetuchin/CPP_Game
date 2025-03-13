644eb7145fa3a606ccfe36165da1a1212859026d
Mazer1x <tetuhin@inbox.ru>
2025-03-13 18:47:21 +0300

    added 2 more enemy types

--- main.cpp
+++ main.cpp
@@ -10,14 +10,14 @@ String TileMap[H] = {
     "                                        ",
     "B                                      B",
     "B                                B     B",
-    "B                                      B",
-    "B                                      B",
-    "B         0000                BBBB     B",
+    "B                 F                    B",
+    "B                           E          B",
+    "B         0000         BBBBBBBBBBB     B",
     "B                                B     B",
     "BBB                              B     B",
     "B              BB                BB    B",
-    "B              BB                      B",
-    "B    B         BB         BB      E    B", 
+    "B              BB         J            B",
+    "B    B         BB         BB           B", 
     "BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB",
 };
 
@@ -66,36 +66,37 @@ class Bullet : public Entity {
     }        
 };
 
-
 class Weapon {
-    Texture bulletTexture;
     float fireDelay;
     float currentDelay;
-    
-public:
-    Weapon(float delay) 
-        : fireDelay(delay), currentDelay(0) {}
 
-    void setBulletTexture(Texture &texture) {
-        bulletTexture = texture;
-    }
+    public:
+        Texture bulletTexture;
+        Weapon(float delay) 
+            : fireDelay(delay), currentDelay(0) {}
 
-    bool tryFire(std::vector<Bullet*> &bullets, float x, float y, bool direction) {
-        if(currentDelay <= 0) {
-            bullets.push_back(new Bullet(bulletTexture,
-                x + (direction ? 40 : -10), 
-                y + 10, direction, 
-                0.5f, 
-                25));
-            currentDelay = fireDelay;
-            return true;
+        void setBulletTexture(Texture &texture) {
+            bulletTexture = texture;
         }
-        return false;
-    }
 
-    void update(float time) {
-        if(currentDelay > 0) currentDelay -= time;
-    }
+        bool tryFire(std::vector<Entity*>& bullets, float x, float y, bool direction) {
+            if (currentDelay <= 0) {
+                bullets.push_back(new Bullet(bulletTexture,
+                    x + (direction ? 6 : -6),
+                    y + 6,
+                    direction,
+                    0.5f,
+                    25));
+                currentDelay = fireDelay;
+                return true;
+            }
+            return false;
+        }
+        
+
+        void update(float time) {
+            if(currentDelay > 0) currentDelay -= time;
+        }
 };
 
 class Player : public Entity {
@@ -148,9 +149,15 @@ public:
         dx = 0;
     }
 
-    void shoot(std::vector<Bullet*> &bullets) {
-        weapon.tryFire(bullets, rect.left, rect.top, lastAction);
+    void shoot(std::vector<Entity*>& bullets) {
+        Bullet* newBullet = new Bullet(
+            weapon.bulletTexture,
+            rect.left + (lastAction ? 40 : -10),
+            rect.top + 10, lastAction,
+            0.5f, 25);
+        bullets.push_back(static_cast<Entity*>(newBullet));
     }
+    
 
     void Collision(int dir) {
         for (int i = rect.top / 32; i < (rect.top + rect.height) / 32; i++)
@@ -169,44 +176,59 @@ public:
                     }
                 }
                 if (TileMap[i][j] == '0') TileMap[i][j] = ' ';
-            }
+        }
     }
 };
 
 class Enemy : public Entity {
+    public:
+        int armor;
+        int health;
+        float speed;
+        float moveRange;
+        float startX;
+        bool isAlive;
+    
+        Enemy(Texture &image, float x, float y, float w, float h, int armor, float speed, float range)
+            : Entity(image, x, y, w, h), armor(armor), health(100), speed(speed), 
+              moveRange(range), startX(x), isAlive(true) {}
+    
+        virtual void takeDamage(int damage) {
+            health -= (damage - armor);
+            if (health <= 0) isAlive = false;
+        }
+    
+        virtual void update(float time) = 0;
+        virtual void Collision(int dir) = 0;
+};
+    
+class WalkingEnemy : public Enemy {
 public:
-    int armor;
     bool onGround;
-    int health;
-    float speed;
-    float moveRange; 
-    float startX;
 
-    Enemy(Texture &image, float x, float y, float w, float h, int armor, float speed, float range)
-        : Entity(image, x, y, w, h), armor(armor), health(100), speed(speed), moveRange(range), startX(x), onGround(false) {}
-
-    void takeDamage(int damage) {
-        health -= (damage - armor);
-        if (health <= 0) isAlive = false;
-    }
+    WalkingEnemy(Texture &image, float x, float y, float w, float h, int armor, float speed, float range)
+        : Enemy(image, x, y, w, h, armor, speed, range), onGround(false) {
+            sprite.setTextureRect(IntRect(0, 0, 150, 150));
+        }
 
     void update(float time) override {
         float nextX = rect.left + speed * time;
     
         if (nextX < startX - moveRange || nextX > startX + moveRange) {
             speed = -speed;
-            nextX = rect.left + speed * time; 
+            nextX = rect.left + speed * time;
         }
 
         rect.left = nextX;
         Collision(0);
     
-        if (!onGround) dy += 0.0005 * time;
+        if(!onGround) dy += 0.0005 * time;
         rect.top += dy * time;
         onGround = false;
         Collision(1);
     }
-    virtual void Collision(int dir) {
+
+    void Collision(int dir) override {
         for (int i = rect.top / 32; i < (rect.top + rect.height) / 32; i++)
             for (int j = rect.left / 32; j < (rect.left + rect.width) / 32; j++) {
                 if (TileMap[i][j] == 'B') {
@@ -225,39 +247,68 @@ public:
             }
     }
 };
-
-class FlyingEnemy : public Enemy {
+class JumpingEnemy : public WalkingEnemy {
+    private:
+        float jumpTimer;
+        float jumpInterval;
+        float jumpForce;
+    
     public:
-        FlyingEnemy(Texture &image, float x, float y, float w, float h, int armor, float speed, float range)
-            : Enemy(image, x, y, w, h, armor, speed, range) {
-            sprite.setTextureRect(IntRect(247, 583, 8, 8)); // Adjust as needed
+        JumpingEnemy(Texture &image, float x, float y, float w, float h, 
+                    int armor, float speed, float range, float jumpForce = 0.35f)
+            : WalkingEnemy(image, x, y, w, h, armor, speed, 200),
+              jumpForce(jumpForce) {
+            jumpInterval = rand() % 3000 + 2000;
+            jumpTimer = jumpInterval;
+            sprite.setTextureRect(IntRect(0, 0, 200, 300)); 
         }
     
         void update(float time) override {
-            float nextX = rect.left + speed * time;
-    
-            if (nextX < startX - moveRange || nextX > startX + moveRange) {
-                speed = -speed;
-                nextX = rect.left + speed * time;
+            jumpTimer -= time;
+            
+            if(jumpTimer <= 0 && onGround) {
+                dy = -jumpForce;
+                onGround = false;
+
+                jumpInterval = rand() % 3000 + 2000;
+                jumpTimer = jumpInterval;
             }
     
-            rect.left = nextX;
-            Collision(0);
+            WalkingEnemy::update(time);
+        }
+};   
+class FlyingEnemy : public Enemy {
+public:
+    FlyingEnemy(Texture &image, float x, float y, float w, float h, int armor, float speed, float range)
+        : Enemy(image, x, y, w, h, armor, speed, range) {
+        sprite.setTextureRect(IntRect(247, 583, 8, 8));
+    }
+
+    void update(float time) override {
+        float nextX = rect.left + speed * time;
+    
+        if (nextX < startX - moveRange || nextX > startX + moveRange) {
+            speed = -speed;
+            nextX = rect.left + speed * time;
         }
     
-        void Collision(int dir) override {
-            if (dir == 0) {
-                for (int i = rect.top / 32; i < (rect.top + rect.height) / 32; i++) {
-                    for (int j = rect.left / 32; j < (rect.left + rect.width) / 32; j++) {
-                        if (TileMap[i][j] == 'B') {
-                            if (speed > 0) rect.left = j * 32 - rect.width;
-                            else if (speed < 0) rect.left = j * 32 + 32;
-                        }
+        rect.left = nextX;
+        Collision(0);
+    }
+
+    void Collision(int dir) override {
+        if (dir == 0) {
+            for (int i = rect.top / 32; i < (rect.top + rect.height) / 32; i++) {
+                for (int j = rect.left / 32; j < (rect.left + rect.width) / 32; j++) {
+                    if (TileMap[i][j] == 'B') {
+                        if (speed > 0) rect.left = j * 32 - rect.width;
+                        else if (speed < 0) rect.left = j * 32 + 32;
                     }
                 }
             }
         }
-    };
+    }
+};
     
 int main() {
     RenderWindow window(VideoMode(800, 600), "Dakaraima!", Style::Titlebar | Style::Close);
@@ -268,29 +319,34 @@ int main() {
     camera.setCenter(400, 300);
     window.setView(camera);
 
-    Texture playerTex;
+    Texture playerTex, enemyTex, bulletTex, fTex;
     playerTex.loadFromFile("assets/fang.png");
-
-    Texture enemyTex;
     enemyTex.loadFromFile("assets/foes.png");
-
-    Texture bulletTex;
+    fTex.loadFromFile("assets/fang.png");
     bulletTex.loadFromFile("assets/fang.png");
 
     Weapon weapon(200.0f);
     weapon.setBulletTexture(bulletTex);
-
     Player player(playerTex, weapon);
 
-    std::vector<Bullet*> bullets;
-    std::vector<Enemy*> enemies;
+    std::vector<Entity*> entities;
+    entities.push_back(&player);
 
+    //! Enemies spawn, after spawning an enemy, game creates an empty space in spawn. point
     for (int i = 0; i < H; i++) {
         for (int j = 0; j < W; j++) {
             if (TileMap[i][j] == 'E') {
-                enemies.push_back(new Enemy(enemyTex, j * 32, i * 32, 40, 50, 5, 0.05f, 100.0f));
-                TileMap[i][j] = ' '; // Remove 'E' from the map after creating the enemy
+                entities.push_back(new WalkingEnemy(enemyTex, j*32, i*32, 40, 50, 5, 0.05f, 100.0f));
+                TileMap[i][j] = ' ';
             }
+            if (TileMap[i][j] == 'F') {
+                entities.push_back(new FlyingEnemy(fTex, j * 32, i * 32, 40, 50, 5, 0.05f, 100.0f));
+                TileMap[i][j] = ' ';
+            }
+            if (TileMap[i][j] == 'J') {
+                entities.push_back(new JumpingEnemy(enemyTex, j*32, i*32, 40, 50, 5, 0.05f, 100.0f, 0.3f));
+                TileMap[i][j] = ' ';
+            }     
         }
     }
 
@@ -301,88 +357,83 @@ int main() {
         float time = clock.getElapsedTime().asMicroseconds();
         clock.restart();
         time = time / 700;
-        if(time > 20) time = 20;
+        if (time > 20) time = 20;
 
         Event event;
-        while(window.pollEvent(event)) {
-            if(event.type == Event::Closed)
+        while (window.pollEvent(event)) {
+            if (event.type == Event::Closed)
                 window.close();
         }
-
-        if(Keyboard::isKeyPressed(Keyboard::Left)) {
-            player.dx = -0.1;
+        //! -------------------------- Keyboard actions -----------------------
+        if (Keyboard::isKeyPressed(Keyboard::Left)) player.dx = -0.1;
+        if (Keyboard::isKeyPressed(Keyboard::Right)) player.dx = 0.1;
+        if (Keyboard::isKeyPressed(Keyboard::Up) && player.onGround) {
+            player.dy = -0.35;
+            player.onGround = false;
         }
 
-        if(Keyboard::isKeyPressed(Keyboard::Right)) {
-            player.dx = 0.1;
-        }
-
-        if(Keyboard::isKeyPressed(Keyboard::Up)) {
-            if(player.onGround) {
-                player.dy = -0.35; 
-                player.onGround = false;
+        if (Keyboard::isKeyPressed(Keyboard::Space)) {
+            if (player.weapon.tryFire(entities, player.rect.left + (player.lastAction ? 40 : -10),
+                                        player.rect.top + 10, player.lastAction)) {
             }
         }
 
-        if(Keyboard::isKeyPressed(Keyboard::Space)) {
-            player.shoot(bullets);
-        }
+        //! -------------------------- Game cycle logic ---------------------------
+        for (auto it = entities.begin(); it != entities.end();) {
+            Entity* entity = *it;
+            entity->update(time);
 
-        player.update(time);
-
-        for (auto& enemy : enemies) {
-            enemy->update(time);
-        }
-
-        for(auto it = bullets.begin(); it != bullets.end();) {
-            (*it)->update(time);
-
-            bool bulletHit = false;
-            for (auto& enemy : enemies) {
-                if (enemy->isAlive && (*it)->rect.intersects(enemy->rect)) {
-                    enemy->takeDamage((*it)->damage);
-                    bulletHit = true;
-                    break;
+            if (auto* bullet = dynamic_cast<Bullet*>(entity)) {
+                bool bulletHit = false;
+                for (auto* enemy : entities) {
+                    if (auto* enemyPtr = dynamic_cast<Enemy*>(enemy)) {
+                         //! Bullet collision logic
+                        if (enemyPtr->isAlive && bullet->rect.intersects(enemyPtr->rect)) {
+                            enemyPtr->takeDamage(bullet->damage);
+                            bulletHit = true;
+                            break;
+                        }
+                    }
+                }
+                //! if bullet hits an entity, bullet disappears
+                if (bulletHit || !bullet->isAlive) {
+                    delete bullet;
+                    it = entities.erase(it);
+                    continue;
                 }
             }
-
-            if (bulletHit || !(*it)->isAlive) {
-                delete *it;
-                it = bullets.erase(it);
-            } else {
-                ++it;
+            //! if entity is alive, it draws on a map
+            if (auto* enemy = dynamic_cast<Enemy*>(entity); enemy && !enemy->isAlive) {
+                delete enemy;
+                it = entities.erase(it);
+                continue;
             }
+
+            ++it;
         }
 
-        camera.setCenter(player.rect.left + player.rect.width / 2, 
-                        player.rect.top + player.rect.height / 2 - 70);
+        camera.setCenter(player.rect.left + player.rect.width / 2,
+                            player.rect.top + player.rect.height / 2 - 70);
         window.setView(camera);
         window.clear(Color::White);
 
-        for(int i = 0; i < H; i++) {
-            for(int j = 0; j < W; j++) {
-                if(TileMap[i][j] == 'B') rectangle.setFillColor(Color::Black);
-                if(TileMap[i][j] == '0') rectangle.setFillColor(Color::Green);
-                if(TileMap[i][j] == ' ') continue;
-                rectangle.setPosition(j*32, i*32);
+        for (int i = 0; i < H; i++) {
+            for (int j = 0; j < W; j++) {
+                if (TileMap[i][j] == 'B') rectangle.setFillColor(Color::Black);
+                if (TileMap[i][j] == '0') rectangle.setFillColor(Color::Green);
+                if (TileMap[i][j] == ' ') continue;
+                rectangle.setPosition(j * 32, i * 32);
                 window.draw(rectangle);
             }
         }
 
-        for(auto &bullet : bullets) {
-            bullet->draw(window);
-        }
-
-        player.draw(window);
-
-        for (auto& enemy : enemies) {
-            if (enemy->isAlive) enemy->draw(window);
-        }
+        for (auto& entity : entities) entity->draw(window);
 
         window.display();
     }
 
-    for(auto &bullet : bullets) delete bullet;
-    for(auto &enemy : enemies) delete enemy;
+    for (auto& entity : entities) {
+        if (entity != &player) delete entity;
+    }
     return 0;
 }
